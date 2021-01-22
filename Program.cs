774c90ce93227298d3d531fc7345a20867f55d04
TSG <TSG@TSGAC>
2021-01-22 11:29:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practicing while/do while loops
+            Console.WriteLine("Please enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            int Begining = 0;
+
+            //Is 0 greater then or equil to the user's input
+            while (Begining <= userInput)
+            {
+                Console.WriteLine(Begining);
+                Begining = Begining + 2;
+            }
+        }
+    }
+}
